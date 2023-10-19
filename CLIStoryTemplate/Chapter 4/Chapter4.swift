d111ca75c8345aab3ed5ff5cea68aa77f0a7d16f
Asif <md.mushfikhasanasif@gmail.com>
2023-10-19 15:41:53 -0400

    fixed typo

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -6,5 +6,5 @@
 import Foundation
 
 func chapterFour() {
-  print ("Moral of thr story is be kind to people and have sympathy for those that are different.")  // Your portion of the story goes here
+  print ("Moral of the story is be kind to people and have sympathy for those that are different.")  // Your portion of the story goes here
 }
