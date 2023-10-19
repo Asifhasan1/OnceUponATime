3cc1bca7095fbb94574f4a67e9d9e57526870920
SyedaH <Syeda.hussain13@gmail.com>
2023-10-19 15:28:19 -0400

    Name

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -8,4 +8,6 @@ import Foundation
 func chapterOne() {
     // Your portion of the story goes here
     print ("Once upon a time there was a school full of dinosaurs")
+    let name: String = "Lisa"
+    print ("There was a new student \(name)")
 }
