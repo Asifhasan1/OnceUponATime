399c12b61b9fba099f38d71560a628168b93006e
Char <staffcha@yahoo.com>
2023-10-25 16:32:37 -0400

    update chp3

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,12 +6,27 @@
 import Foundation
 
 func chapterThree() {
-   
-        //        let text: String
+    
+//    func tellStory() {
         print ("Chapter 3")
+        print ("\(friendName) came to \(maincharacterName) to tell her that only she could fit in the rocket to save everyone")
+        print ("\(maincharacterName) said to \(friendName) I'm not sure that I want to save everyone. ")
+        print ("\(friendName) said to \(maincharacterName) I understand and I will be your friend regardless.")
         
+//        isSave = Bool.random()
+//
+//        if isSave{
+//            print(\(maincharacterName) will )
             
+            
+          
             
         }
+    
+    
+
+            
+            
+        
         
         
