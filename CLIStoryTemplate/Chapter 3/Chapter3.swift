35ebfee1f4d22031abcc969fa3e79625ea0fc133
Char <staffcha@yahoo.com>
2023-10-25 16:49:08 -0400

    update chp3

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -7,26 +7,38 @@ import Foundation
 
 func chapterThree() {
     
-//    func tellStory() {
-        print ("Chapter 3")
-        print ("\(friendName) came to \(maincharacterName) to tell her that only she could fit in the rocket to save everyone")
-        print ("\(maincharacterName) said to \(friendName) I'm not sure that I want to save everyone. ")
-        print ("\(friendName) said to \(maincharacterName) I understand and I will be your friend regardless.")
-        
-//        isSave = Bool.random()
-//
-//        if isSave{
-//            print(\(maincharacterName) will )
-            
-            
-          
-            
+    //    func tellStory() {
+    print ("Chapter 3")
+    print ("\(friendName) came to \(maincharacterName) to tell her that only she could fit in the rocket to save everyone")
+    print ("\(maincharacterName) said to \(friendName) I'm not sure that I want to save everyone. ")
+    print ("\(friendName) said to \(maincharacterName) I understand and I will be your friend regardless.")
+    
+    var isSave: Bool = true
+    if isSave {
+        if isSave {
+            print("\(maincharacterName) decided to go to the rocket and save \(friendName) and the \(bullyName) because she did not want to lose her dear friend \(friendName) .")
+//        } else {
+//            print("\(maincharacterName) decided that she wanted to leave Dinosaur Island and move on from being bullied. She went to the rocket and changed to coordinates to an unknown planet.")
         }
+        //
+        //    }
+        //    if isRaining == true {
+        //        print("stay")
+        //        isSave = Bool.random()
+        //
+        //        if isSave{
+        //            print(\(maincharacterName) will )
+        
+        
+        
+        
+    }
     
     
-
-            
-            
-        
-        
-        
+    
+    
+    
+    
+    
+    
+}
