fe55dcbc653bb00bc15aaa65e4921ae9a1cf0e38
Asif <md.mushfikhasanasif@gmail.com>
2023-10-26 13:43:43 -0400

    conditions & bool

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -7,12 +7,23 @@ import Foundation
 
 func chapterFour() {
     
-    let story = "Moral of the story is"
-    let action = "be kind to people and have sympathy for those that are different."
-    
+    func kindnessMoral() {
 
-    
-    let message = "\(story) \(action)"
-    print(message)
+        let moralMessage = "Moral of the story is"
+        let kindActions = "be kind to people and have sympathy for those who are different."
 
+        var isKind: Bool = true
+
+        func displayKindnessMessage(_ isKind: Bool) {
+            if isKind {
+                print("\(moralMessage) \(kindActions)")
+                print("You never know what someone could be going through, and being rude/harsh towards them will only make it worse.")
+                print("If you're kind towards people, they'll be kind towards you, and maybe someday you might need their help with something, and being kind will only help you in that situation.")
+            } else {
+                print("In this story, kindness was the key to better outcomes.")
+            }
+        }
+        displayKindnessMessage(isKind)
+    }
+    kindnessMoral()
 }
